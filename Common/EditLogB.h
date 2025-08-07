410bad30851ff77116352412f31626efbf9c8366
jkchoi2 <send_jk@nate.com>
2025-08-07 14:53:26 +0900

    v1.2 update

---
+++ Common/EditLogB.h
@@ -0,0 +1,38 @@
+#pragma once 
+#include <afxwin.h>
+#include <vector>
+#include <string>
+#include "LockBlock.h"
+#include "EditEx.h"
+
+
+using namespace std;
+
+class CEditLogB : public CEditEx	//CEdit
+{
+public:
+	CEditLogB();
+	virtual ~CEditLogB();
+
+private:
+	bool _init;
+	int _maxLine;
+	int _textLength;
+
+	vector<string> _logList;
+
+	CBrush _brush;
+	CFont _font;
+	CLock _lock;
+
+public:
+	int PrintLog (const char* msg); 
+
+	void Update ();
+	void SetMaxLines (int maxLine) { _maxLine = maxLine; _init = false; }
+
+	DECLARE_MESSAGE_MAP()
+protected:
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	afx_msg void OnDestroy();
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# SerialPortMon
+
+serialport, UDP/TCP Server and Client program.
+
+처음에 https://blog.daum.net/pg365/276 여기서 공개된 프로그램인데, 더이상 링크가 유효하지않고 2014년이후 업데이트가 없습니다.  
+원본은 VS2008 WIN32로 개발된 소스 입니다.
+
+
+
+## 수정내용 
+ - 2025.08.07
+ - Version변경: 1.15 --> 1.2
+ - Fixedsys폰트가 적용안되는 버그 수정 및 폰트 변경 기능 추가
+
